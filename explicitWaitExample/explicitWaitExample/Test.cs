269ab1df3fee35fba88d2176c33500c95e53b93c
Arseni Vasenda <vasenda2000@yandex.ru>
2022-02-18 16:42:54 +0300

    Initial commit

---
+++ explicitWaitExample/explicitWaitExample/Test.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace explicitWaitExample
+{
+    public class Test
+    {
+        private IWebDriver Driver;
+        
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            Driver = new ChromeDriver();
+        }
+
+        [Test]
+        void Test1()
+        {
+            //some arrange
+
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(int.MaxValue));
+            IWebElement someElement = Driver.FindElement(By.Id("someID"));
+            string initialText = someElement.Text;
+
+            wait.Until<bool>((d) =>
+            {
+                if (someElement.Text != initialText)
+                {
+                    return true;
+                }
+                return false;
+            });
+
+            //some assertions
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+    }
+}
